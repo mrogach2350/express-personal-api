c1171031194ba72ab5487516ce81e03511cd621c
mrogach2350 <m.rogach@gmail.com>
2016-09-02 11:37:56 -0700

    adds seed.js filefor boardgames

--- seed.js
+++ seed.js
@@ -13,3 +13,72 @@
 //   console.log("Created new campsite", campsite._id)
 //   process.exit(); // we're all done! Exit the program.
 // })
+
+var db = require('./models');
+
+var boardgames_list = [
+  {
+  name: 'Power Grid',
+  minPlayers = 2,
+  maxPlayers = 6,
+  timeToPlay = 120,
+  singlePlayerOption = false,
+  releaseYear = 2004,
+  gameType = 'Strategy'
+  },
+  {
+  name: 'Puerto Rico',
+  minPlayers = 2,
+  maxPlayers = 5,
+  timeToPlay = 150,
+  singlePlayerOption = false,
+  releaseYear = 2002,
+  gameType = 'Strategy'
+  },
+  {
+  name: 'Cranium',
+  minPlayers = 4,
+  maxPlayers = 16,
+  timeToPlay = 60,
+  singlePlayerOption = false,
+  releaseYear = 1998,
+  gameType = 'Party'
+  },
+  {
+  name: 'Scoville',
+  minPlayers = 2,
+  maxPlayers = 6,
+  timeToPlay = 90,
+  singlePlayerOption = false,
+  releaseYear = 2014,
+  gameType = 'Strategy'
+  },
+  {
+  name: 'Agricola',
+  minPlayers = 1,
+  maxPlayers = 5,
+  timeToPlay = 150,
+  singlePlayerOption = true,
+  releaseYear = 2007,
+  gameType = 'Strategy'
+  },
+  {
+  name: 'Betrayal at House on the Hill',
+  minPlayers = 3,
+  maxPlayers = 6,
+  timeToPlay = 60,
+  singlePlayerOption = false,
+  releaseYear = 2004,
+  gameType = 'Thematic'
+  },
+]
+
+db.BoardGame.remove({}, function(err, boardgames){
+
+  db.BoardGame.create(boardgames_list, function(err, boardgames){
+      if(err){return console.log(err);}
+        console.log("created", boardgames.length, 'boardgames');
+        process.exit();
+  });
+
+});
