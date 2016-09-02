5951c599f3f87356a9349035988c074650e027b3
mrogach2350 <m.rogach@gmail.com>
2016-09-02 14:33:38 -0700

    adds profile data to index.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,37 @@
 console.log("Sanity Check: JS is working!");
-
+var template;
+var $profile;
+var $gamesList;
+var profile;
+var allGames = [];
 $(document).ready(function(){
 
-// your code
+  $profile = $('#profileTarget');
+
+  var source = $('#profile-template').html();
+  template = Handlebars.compile(source);
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/profile',
+    success: profileHandleSuccess,
+    error: profileHandleError
+  });
+
 
 });
+function renderProfile (){
+  $profile.empty();
+  var profileHtml = template({profile: profile});
+  $profile.append(profileHtml);
+}
+
+function profileHandleSuccess(json){
+  profile = json;
+  renderProfile();
+}
+
+function profileHandleError(e){
+  console.log('uh oh');
+  $('#profileTarget').text('Failed to load profile data.');
+}
