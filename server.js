12951088a5bddd18b2c1a0b8fe4f3bc69d79a058
mrogach2350 <m.rogach@gmail.com>
2016-09-02 08:49:12 -0700

    hardcodes profile and updates ProfileSchema

--- server.js
+++ server.js
@@ -58,15 +58,20 @@ app.get('/api', function api_index(req, res) {
 });
 
 app.get('/api/profile', function api_index(req, res){
-  db.Profile.find({})
-    .exec(function(err, books){
-      if (err) {
-        res.status(500).send(err);
-        return;
-      }
-      res.json(books);
-    });
+  var profile = {
+      name: 'Michael E. Rogachevsky',
+      githubLink: 'https://github.com/mrogach2350',
+      githubProfileImage: "https://avatars1.githubusercontent.com/u/19850983?v=3&s=460",
+      personalSiteLink: "mrogach2350.github.io",
+      currentCity: 'San Francisco',
+      pets: [{name:'Nate', type: 'Dog', breed: 'Shepard Mix', owner: 'Jim'},
+      {name:'Stella', type: 'Dog', breed: 'Lab', owner: 'Rachel'},
+      {name:'Putin', type: 'Dog', breed: 'Shepard/Husky Mix', owner: 'Anton'},
+      {name:'Crookshanks', type: 'Cat', breed: 'Domestic Shorthair', owner: 'Andrew'}]
+  }
+      res.json(profile);
 });
+
 
 /**********
  * SERVER *
